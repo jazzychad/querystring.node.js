511d6a2941b04d9fe756c8fdb707320971d48411
isaacs <i@foohack.com>
2009-12-15 16:53:54 -0800

    Some more traces of merge collisions.  Corrected.

--- querystring-parse.js
+++ querystring-parse.js
@@ -42,7 +42,6 @@ exports.parse = querystring_parse;
  * @for querystring
  * @static
  */
-<<<<<<< HEAD
 function querystring_parse (qs, sep, eq, unesc) {
   return qs.split(sep || "&")
     .map(pieceParser(eq || "=", unesc || unescape))
@@ -51,22 +50,6 @@ function querystring_parse (qs, sep, eq, unesc) {
 
 function unescape (s) {
     return decodeURIComponent(s.replace(/\+/g, ' '));
-=======
-var parse = function (qs, sep, eq) {
-  // wouldn't Array(qs.split()).map(pieceParser(eq)).reduce(mergeParams) be prettier?
-  return util.reduce(
-		     util.map(
-			      qs.split(sep || "&"),
-			      pieceParser(eq || "=")
-			      ),
-{},
-		     mergeParams
-		     );
-};
-
-var unescape = function (s) {
-  return decodeURIComponent(s.replace(/\+/g, ' '));
->>>>>>> dfaf3130e7ad0c25f7464dc46bb862c9f0bfb9ee
 };
 
 
@@ -79,40 +62,23 @@ var unescape = function (s) {
 // return parse(foo[bar], [{bla:"baz"}])
 // return parse(foo, {bar:[{bla:"baz"}]})
 // return {foo:{bar:[{bla:"baz"}]}}
-<<<<<<< HEAD
 function pieceParser (eq, unesc) {
   return function parsePiece (key, val) {
-    
-=======
-var pieceParser = function (eq) {
-  return function parsePiece (key, val) {
->>>>>>> dfaf3130e7ad0c25f7464dc46bb862c9f0bfb9ee
     if (arguments.length !== 2) {
       // key=val, called from the map/reduce
       key = key.split(eq);
       return parsePiece(
-<<<<<<< HEAD
         unesc(key.shift()),
         unesc(key.join(eq))
       );
-=======
-			unescape(key.shift()),
-			unescape(key.join(eq))
-			);
->>>>>>> dfaf3130e7ad0c25f7464dc46bb862c9f0bfb9ee
     }
     key = key.replace(/^\s+|\s+$/g, '');
     if (util.isString(val)) {
       val = val.replace(/^\s+|\s+$/g, '');
       // convert numerals to numbers
       if (!isNaN(val)) {
-<<<<<<< HEAD
         var numVal = +val;
         if (val === numVal.toString(10)) val = numVal;
-=======
-	var numVal = +val;
-	if (val === numVal.toString(10)) val = numVal;
->>>>>>> dfaf3130e7ad0c25f7464dc46bb862c9f0bfb9ee
       }
     }
     var sliced = /(.*)\[([^\]]*)\]$/.exec(key);
@@ -122,12 +88,8 @@ var pieceParser = function (eq) {
       return ret;
     }
     // ["foo[][bar][][baz]", "foo[][bar][]", "baz"]
-<<<<<<< HEAD
     var tail = sliced[2],
       head = sliced[1];
-=======
-    var tail = sliced[2], head = sliced[1];
->>>>>>> dfaf3130e7ad0c25f7464dc46bb862c9f0bfb9ee
 
     // array: key[]=val
     if (!tail) return parsePiece(head, [val]);
@@ -141,7 +103,6 @@ var pieceParser = function (eq) {
 
 // the reducer function that merges each query piece together into one set of params
 function mergeParams (params, addition) {
-<<<<<<< HEAD
 	var ret;
 			
 	if (!params){
@@ -158,39 +119,14 @@ function mergeParams (params, addition) {
 		ret = mergeObjects(params, addition);
 	}
 	return ret;
-=======
-  var ret;
-            
-  if (!params){
-    // if it's uncontested, then just return the addition.
-    ret = addition;
-  } else if (util.isArray(params)) {
-    // if the existing value is an array, then concat it.
-    ret = params.concat(addition);
-  } else if (!util.isObject(params) || !util.isObject(addition)) {
-    // if the existing value is not an array, and either are not objects, arrayify it.      
-    ret = [params].concat(addition);
-  } else {
-    // else merge them as objects, which is a little more complex
-    ret = mergeObjects(params, addition);
-  }
-  return ret;
->>>>>>> dfaf3130e7ad0c25f7464dc46bb862c9f0bfb9ee
 };
 
 
 // Merge two *objects* together. If this is called, we've already ruled
 // out the simple cases, and need to do the for-in business.
 function mergeObjects (params, addition) {
-<<<<<<< HEAD
   for (var i in addition) if (i && addition.hasOwnProperty(i)) {
     params[i] = mergeParams(params[i], addition[i]);
-=======
-  for (var i in addition) {
-    if (i && addition.hasOwnProperty(i)) {
-      params[i] = mergeParams(params[i], addition[i]);
-    }
->>>>>>> dfaf3130e7ad0c25f7464dc46bb862c9f0bfb9ee
   }
   return params;
 };
